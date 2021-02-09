180df7b07e0e81b12749304dd7b15e55d963079c
hongyunfei <34985241+hongyunfei@users.noreply.github.com>
2021-02-09 14:43:54 +0800

    Add files via upload

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -1,7 +1,9 @@
 
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
-
+if "refreshtokenVal" in os.environ and os.environ["refreshtokenVal"]:
+        BARK = os.environ["refreshtokenVal"]
+		
 let refreshtokenVal= ``
 
 
