839ea05c4d2da4e1c473256a7086dfef85d48b3a
hongyunfei <34985241+hongyunfei@users.noreply.github.com>
2021-02-27 16:49:02 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,11 +5,11 @@
 #京东超级兑奖
 59 23,11 * * * sleep 59 && node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 #京东超级兑奖
-01 00,12 * * * sleep 59 && node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+01 00,12 * * * sleep 01 && node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 #宠旺旺兑奖
 59 23,11,7,15 * * * sleep 59 && node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 #宠旺旺兑奖
-01 00,12,8,16 * * * sleep 59 && node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
+01 00,12,8,16 * * * sleep 01 && node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 #笑谱
 */30 8-23 * * * node /scripts/iboxpay.js >> /scripts/logs/iboxpay.log 2>&1
 #步步宝
