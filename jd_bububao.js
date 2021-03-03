246dc6d2a535e1f15236d047f8aa603f1232852c
hongyunfei <34985241+hongyunfei@users.noreply.github.com>
2021-03-03 21:53:51 +0800

    Update jd_bububao.js

--- jd_bububao.js
+++ jd_bububao.js
@@ -339,7 +339,7 @@ function msgShow() {
         if (notifyInterval == 3 && (nowTimes.getHours() === 6 || nowTimes.getHours() === 12 || nowTimes.getHours() === 18 || nowTimes.getHours() === 23) && (nowTimes.getMinutes() >= 0 && nowTimes.getMinutes() <= 10)) {
             $.msg($.name, ``, $.message);
         }
-        if (notifyttt == 1 && $.isNode() && (nowTimes.getHours() === 12 || nowTimes.getHours() === 21) && (nowTimes.getMinutes() >= 15 && nowTimes.getMinutes() <= 35))
+        if (notifyttt == 1 && $.isNode() && (nowTimes.getHours() === 12 || nowTimes.getHours() === 23) && (nowTimes.getMinutes() >= 45 && nowTimes.getMinutes() <= 50))
             await notify.sendNotify($.name, $.message);
         resolve()
     })
